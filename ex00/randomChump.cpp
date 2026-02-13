7abd363e48747b5ade88aca30f6685f867010198
TerraMint <studentbreus@gmail.com>
2026-02-13 15:46:07 +0100

    feat: adding randomChump function

---
+++ ex00/randomChump.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/13 15:41:36 by zivanov           #+#    #+#             */
+/*   Updated: 2026/02/13 15:42:42 by zivanov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void	randomChump(std::string name)
+{
+	Zombie	chump;
+
+	chump.setName(name);
+	chump.announce();
+}
+
