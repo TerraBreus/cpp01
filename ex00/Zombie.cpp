d42e835d01eac80a8d05b5bbc0dd95252fb0d6c3
TerraMint <studentbreus@gmail.com>
2026-02-13 15:45:11 +0100

    feat: added setName, constructor and destructor to Zombie class

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,14 +6,28 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 14:52:20 by zivanov           #+#    #+#             */
-/*   Updated: 2026/02/13 14:53:34 by zivanov          ###   ########.fr       */
+/*   Updated: 2026/02/13 15:44:28 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 #include <iostream>
 
+Zombie::Zombie()
+{
+}
+
+Zombie::~Zombie()
+{
+	std::cout << name << " has been destroyed." << std::endl;
+}
+
 void	Zombie::announce(void)
 {
 	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void	Zombie::setName(std::string str)
+{
+	name = str;
 }
