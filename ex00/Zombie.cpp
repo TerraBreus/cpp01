d7e339ec70f29f01e728861994ff898ba4b30803
TerraMint <studentbreus@gmail.com>
2026-02-13 14:55:59 +0100

    feat: initial commit with files touched

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/13 14:52:20 by zivanov           #+#    #+#             */
+/*   Updated: 2026/02/13 14:53:34 by zivanov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+#include <iostream>
+
+void	Zombie::announce(void)
+{
+	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ ex00/Zombie.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/13 14:37:09 by zivanov           #+#    #+#             */
+/*   Updated: 2026/02/13 14:39:12 by zivanov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+# include <string>
+
+class Zombie
+{
+	private:
+		std::string	name;
+	public:
+		void		announce( void );
+};
+
+#endif
