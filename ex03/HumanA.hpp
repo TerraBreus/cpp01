b39dcbb545a8baf42031eeb80fba3c0ef928bf11
TerraBreus <studentbreus@gmail.com>
2026-02-14 14:01:33 +0100

    feat: adding files for ex03

---
+++ ex03/HumanA.hpp
@@ -0,0 +1,16 @@
+#include <string>
+
+#ifndef HUMAN_A_HPP
+# define HUMAN_A_HPP
+
+#include <string>
+#include "Weapon.hpp"
+
+class	HumanA
+{
+	private:
+		Weapon		weapon;
+		std::string	name;
+	public:
+};
+#endif
---
+++ ex03/Weapon.hpp
@@ -0,0 +1,11 @@
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+# include <string>
+
+class	Weapon
+{
+	private:
+		std::string	type;
+	public:
+};
+#endif
