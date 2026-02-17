0318cabcde8ce402459d0a343b56f45705ed4efa
TerraMint <studentbreus@gmail.com>
2026-02-17 11:41:44 +0100

    feat: adding ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <sstream>
 #include <iostream>
 #include <string>
 
@@ -10,8 +11,25 @@ int	sedIsForLosers(char *argv[])
 	std::string			textFromFilename;
 	
 	//open filename and put text in textFromFilename
+	std::ifstream inputFile(filename.c_str());
+	std::stringstream	buffer;
+	buffer << inputFile.rdbuf();
+	std::string			inputString;
+	inputString = buffer.str();
+	std::cout << inputString;
 	//replace every instance of s1 to s2 in textFromFilename
+	size_t	pos = 0;
+	while ((pos = inputString.find(s1, pos)) != std::string::npos)
+	{
+		inputString.erase(pos, s1.length());
+		inputString.insert(pos, s2);
+		pos += s2.length();
+	}
+	std::cout << inputString;
 	//create textForNewFile as `filename.replace` and put textFromFilename into it.
+	std::string		outputFilename = filename + ".replace";
+	std::ofstream	outputFile(outputFilename.c_str());
+	outputFile << inputString;
 	
 	return (0);
 }
@@ -19,7 +37,9 @@ int	sedIsForLosers(char *argv[])
 int	main(int argc, char *argv[])
 {
 	if (argc != 4)
+	{
+		std::cerr << "Usage: ./sedIsForLosers filename s1 s2" << std::endl;
 		return (-1);
-	else if (sedIsForLosers(argv) != 0)
-		return (-1);
+	}
+	return (sedIsForLosers(argv));
 }
