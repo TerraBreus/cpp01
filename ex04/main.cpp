d5b486434eb52c77d0e58c63ab6da4641dfc36f0
TerraBreus <studentbreus@gmail.com>
2026-02-18 18:41:15 +0100

    refactor: putting sections of code into different file

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,33 +3,23 @@
 #include <iostream>
 #include <string>
 
-
+std::string	openFile(std::string filename);
 std::string	replaceString(std::string oldString, std::string s1, std::string s2);
+int			createAndWriteOutput(std::string outputString, std::string filename);
 
 int	sedIsForLosers(char *argv[])
 {
 	std::string			filename = argv[1];
 	const std::string	s1 = argv[2];
 	const std::string	s2 = argv[3];
-	std::string			textFromFilename;
-	
-	//open filename and put text in textFromFilename
-	std::ifstream inputFile(filename.c_str());
-	std::stringstream	buffer;
-	buffer << inputFile.rdbuf();
 	std::string			inputString;
-	inputString = buffer.str();
-	std::cout << inputString;
+	std::string			outputString;
 
-	std::string	outputString;
+	inputString = openFile(filename);
+	if (inputString.empty())
+		return (-1);
 	outputString = replaceString(inputString, s1, s2);
-	//create textForNewFile as `filename.replace` and put textFromFilename into it.
-	std::string		outputFilename = filename + ".replace";
-	std::ofstream	outputFile(outputFilename.c_str());
-	std::cout << outputString;
-	outputFile << outputString;
-	
-	return (0);
+	return (createAndWriteOutput(outputString, filename));
 }
 
 int	main(int argc, char *argv[])
